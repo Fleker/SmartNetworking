197769c6cd96295c68ef40daa3e29589a3b43bf9
Aaron Yangello <ayangello@gmail.com>
2016-05-23 21:04:52 -0400

    Added Layout for User Contact Profile

---
+++ app/src/main/java/org/rowanieee/smartnetworking/model/UserInfoActivity.java
@@ -0,0 +1,17 @@
+package org.rowanieee.smartnetworking.model;
+
+/**
+ * Created by Aaron Yangello on 5/17/2016.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;import org.rowanieee.smartnetworking.R;
+
+public class UserInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userinfo);
+    }
+}
